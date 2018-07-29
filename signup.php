67bd59bbd5f1247ac82fef2c81bd0f2c344374ac
Jordan Sadiwa <jordan@infinitedev.works>
2018-07-29 21:48:58 +0830

    Fixed login design

--- signup.php
+++ signup.php
@@ -7,25 +7,25 @@
 		<form method="post" id="signup" action="">
 			<input type="hidden" name="signup" value="1">
 			<label>Username
-				<input type="text" name="username" required placeholder="Username..."/>
+				<input type="text" class="form-control" name="username" required placeholder="Username..."/>
 			</label>
 			<label>Password
-				<input type="password" name="password1" required placeholder="Password..."/>
+				<input type="password" class="form-control" name="password1" required placeholder="Password..."/>
 			</label>
 			<label>Retype Password
-				<input type="password" name="password2" required placeholder="Password..."/>
+				<input type="password" class="form-control" name="password2" required placeholder="Password..."/>
 			</label>
 			<h5>User Type</h5>
 			<label>Applicant
-				<input type="radio" name="usertype" checked value="applicant">
+				<input type="radio" class="form-control" name="usertype" checked value="applicant">
 			</label>
 			<label>Employer
 				<input type="radio" name="usertype" value="employer">
 			</label>
-			<input type="submit" value="next">
+			<input type="submit" class="btn btn-primary" value="next">
 		</form>
 		<form method="post" id="userinfo">
-			<input type="hidden" name="userinfo" value="1">
+			<input type="hidden" name="userinfo"  value="1">
 		</form>
 	</section>
     <script src="js/jquery.js"></script>
